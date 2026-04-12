a760431431b050eee201f6c887470e6bc73691d5
jeremyecker <jeremy@resteasypest.com>
2026-04-12 07:06:45 -0400

    Add Ahrefs Web Analytics tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,8 @@ export default function RootLayout({
             __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","vzxjg8jewg");`
           }}
         />
+        {/* Ahrefs Web Analytics */}
+        <script src="https://analytics.ahrefs.com/analytics.js" data-key="eGtdysc9Tojf5nOQdjf++g" async />
       </head>
       <body className="min-h-screen flex flex-col">
         <TopBar />
